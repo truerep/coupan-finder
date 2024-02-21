c7f14d7090df3c351933b7d2a46122f3969bafc3
tradestealer999 <112725984+truerep@users.noreply.github.com>
2024-02-21 23:52:33 +0530

    --

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleAccountMenu() {
+  document.querySelector('.account-cta').classList.toggle('active');
+}
+
+function toggleSearchBar() {
+  document.querySelector('.search-bar-wrapper').classList.toggle('active');
+}
+
+function toggleSidebar() {
+  document.querySelector('.nav-main').classList.toggle('active');
+}
+
+function toggleFilters() {
+  document.querySelector('.filter-options').classList.toggle('active');
+}
